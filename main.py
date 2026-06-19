d45f639cd0d6a1896ca7d6e6c0da906ebdef2a7f
José Enrique <josesanchezb7@gmail.com>
2026-06-19 09:59:32 +0100

    Debug motivo pausa

--- main.py
+++ main.py
@@ -343,7 +343,7 @@ def panel_admin(token: str = None):
         
 @app.get("/pausa")
 def pausa(empleado_id: str, motivo: str = None):
-
+    print("MOTIVO RECIBIDO:", motivo)
     conn = database.conectar()
     cursor = conn.cursor()
 
